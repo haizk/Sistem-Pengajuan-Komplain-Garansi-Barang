97fcf4b725d379289e2848f140a3c1dfbedac115
adelinefellita <adelinefellita@gmail.com>
2023-05-29 16:58:52 +0700

    isi tabel riwayat pengajuan garansi

--- database/migrations/2023_05_29_091031_create_riwayat_pengajuan_garansi_table.php
+++ database/migrations/2023_05_29_091031_create_riwayat_pengajuan_garansi_table.php
@@ -12,7 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('riwayat_pengajuan_garansi', function (Blueprint $table) {
-            $table->id();
+            $table->increments("ID_RiwayatPengajuanGaransi");
+            $table->string("Pengajuan");
+            $table->date("TanggalPengajuan");
+            $table->date("TanggalSelesai");
+            $table->string("Status");
+            $table->foreign("ID_Admin")
+                ->references('increments')->on('user')->onDelete('cascade');
+            $table->foreign("ID_Pembeli");
+                ->references('increments')->on('ruser')->onDelete('cascade');
+            $table->foreign("ID_Barang");
+                >references('increments')->on('barang')->onDelete('cascade');
             $table->timestamps();
         });
     }
