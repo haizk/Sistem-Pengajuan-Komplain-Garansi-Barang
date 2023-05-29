39e8102b9d57d140d08cccf7c9b57933dc200057
adelinefellita <adelinefellita@gmail.com>
2023-05-29 16:59:58 +0700

    isi riwayat pengajuan garansi

--- database/migrations/2023_05_29_091031_create_riwayat_pengajuan_garansi_table.php
+++ database/migrations/2023_05_29_091031_create_riwayat_pengajuan_garansi_table.php
@@ -13,17 +13,17 @@ return new class extends Migration
     {
         Schema::create('riwayat_pengajuan_garansi', function (Blueprint $table) {
             $table->increments("ID_RiwayatPengajuanGaransi");
-            $table->string("Pengajuan");
-            $table->date("TanggalPengajuan");
-            $table->date("TanggalSelesai");
-            $table->string("Status");
-            $table->foreign("ID_Admin")
-                ->references('increments')->on('user')->onDelete('cascade');
-            $table->foreign("ID_Pembeli");
-                ->references('increments')->on('ruser')->onDelete('cascade');
-            $table->foreign("ID_Barang");
-                >references('increments')->on('barang')->onDelete('cascade');
             $table->timestamps();
+            $table->string("pengajuan");
+            $table->date("tanggal_pengajuan");
+            $table->date("tanggal_selesai");
+            $table->string("status");
+            $table->unsignedBigInteger("id_admin");
+            $table->unsignedBigInteger("id_pembeli");
+            $table->unsignedBigInteger("id_barang");
+            $table->foreign("id_admin")->references('increments')->on('user')->onDelete('cascade');
+            $table->foreign("id_pembeli")->references('increments')->on('user')->onDelete('cascade');
+            $table->foreign("id_barang")->references('increments')->on('barang')->onDelete('cascade');
         });
     }
 
