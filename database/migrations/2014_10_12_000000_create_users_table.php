51c2891ad059df8d3ae958da9456c0212f76fbaf
aniindyta <aninditaputri055@gmail.com>
2023-06-13 14:43:48 +0700

    Uji Coba

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('role');
-            $table->string('nomor_hp');
+            $table->string('role')->default('user');
+            $table->string('nomor_hp')->nullable();
             $table->timestamps();
         });
     }
