27d452dcc3a63aea420954c4f3fe78a7dcb053c4
ftriara <fitriarahmadani67@gmail.com>
2023-07-02 11:48:42 +0700

    template

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -19,11 +25,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'username',
         'email',
         'password',
-        'role',
-        'nomor_hp'
     ];
 
     /**
@@ -34,10 +37,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'id',
-        'email_verified_at',
-        'created_at',
-        'updated_at'
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -47,21 +48,14 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 
-    public function Barang()
-    {
-        return $this->hasMany(Barang::class);
-    }
-
-    public function Riwayat_Pengajuan_Garansi()
-    {
-        return $this->hasMany(Riwayat_Pengajuan_Garansi::class);
-    }
-
-    public function Riwayat_Tindakan()
-    {
-        return $this->hasMany(Riwayat_Tindakan::class);
-    }
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app') --}}
+@extends('layouts.template_default')
 
 @section('content')
 <div class="container">
