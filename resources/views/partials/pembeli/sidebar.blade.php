117f8459cd5db6e6de97a52c64d5f9654298744e
adelinefellita <adelinefellita@gmail.com>
2023-07-06 21:43:43 +0700

    update

---
+++ resources/views/partials/pembeli/sidebar.blade.php
@@ -0,0 +1,27 @@
+<ul class="menu-inner py-1">
+    <hr class="my-2" />
+    <li class="menu-item active">
+        <a href="index1.html" class="menu-link">
+            <i class="menu-icon tf-icons bx bx-home-circle"></i>
+            <div data-i18n="Analytics">Dashboard</div>
+        </a>
+    </li>
+    <li class="menu-item">
+        <a href="form.html" class="menu-link">
+            <i class="menu-icon tf-icons bx bxs-paper-plane"></i>
+            <div data-i18n="Basic">Ajukan Komplain</div>
+        </a>
+    </li>
+    <li class="menu-item">
+        <a href="tables-basic.html" class="menu-link">
+            <i class="menu-icon tf-icons bx bxs-comment-check"></i>
+            <div data-i18n="Tables">Status Pengajuan</div>
+        </a>
+    </li>
+    <li class="menu-item">
+        <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank" class="menu-link">
+            <i class="menu-icon tf-icons bx bxs-log-out"></i>
+            <div data-i18n="Support">Log out</div>
+        </a>
+    </li>
+</ul>
